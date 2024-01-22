1d0fe91762aae847ec4b1699f9235fde27a7fd1a
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-01-22 06:34:26 +0530

    basic layout created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <div>
+      <Navbar/>
+      <Hero/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Contact/>
     </div>
   );
 }
